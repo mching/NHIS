a0d43e739cc73cc6d5fc8a09871cc989337a7bbe
Mike Ching <mikeching@yahoo.com>
2016-07-21 16:11:16 -1000

    figured out how to present population totals by factor variable and proportions.

--- scripts/demographics.R
+++ scripts/demographics.R
@@ -22,37 +22,88 @@ cam.design <-
   )
 
 # Number of children
+nrow(x.sa)
+
 svytotal(
   ~one ,
   cam.design 
 )
 
 # Gender breakdown
-x.sa$sex <- factor(x.sa$sex, labels = c("male", "female"))
+cam.design <- update(cam.design, sex_f_ = factor(x.sa$sex, labels = c("male", "female")))
+
+# 
 table(x.sa$sex)
 prop.table(table(x.sa$sex))
 
-svytotal(~sex, design = cam.design)
-confint(svytotal(~sex, design = cam.design))
+svytotal(~sex_f_, design = cam.design)
+confint(svytotal(~sex_f_, design = cam.design))
 
-svytotal(~sex, design = cam.design)/sum(svytotal(~sex, design = cam.design))
-confint(svytotal(~sex, design = cam.design))/sum(svytotal(~sex, design = cam.design))
+svymean(~I(sex_f_ == "male"), cam.design)
+confint(svymean(~I(sex_f_ == "male"), cam.design))
 
 # Age breakdown
 table(x.sa$age_p)
 prop.table(table(x.sa$age_p))
 
+svymean(~age_p, cam.design)
+svyquantile(~age_p, cam.design, quantiles=c(0.25,0.5,0.75))
+
 # Race
-x.sa$racerpi2 <-factor(x.sa$racerpi2, labels = c("White only",
-                                                 "Black/African American only",
-                                                 "AIAN only", 
-                                                 "Asian only",
-                                                 "Race group not releasable",
-                                                 "Multiple race"))
+race_names <- c("White only",
+                "Black/African American only",
+                "AIAN only", 
+                "Asian only",
+                "Race group not releasable",
+                "Multiple race")
+cam.design <-update(cam.design,
+                    race_ = factor(x.sa$racerpi2, labels = race_names))
+
 table(x.sa$racerpi2)
 
-svytotal(~racerpi2)
+svytotal(~race_, cam.design)
+
+# Helper function to automate the estimation of proportions in the population
+race_extract <- function(race_name) {
+  m <- svymean(~I(race_ == race_name), cam.design)
+  print(race_name)
+  print(m)
+  print(confint(m))
+}
+
+sapply(race_names, race_extract)
 
 # Hispanic
+table(x.sa$hispan_i, useNA = "ifany")
+x.sa$hispan_TF <- ifelse(x.sa$hispan==12, "Not Hispanic", "Hispanic")
+x.sa$hispan_TF <- factor(x.sa$hispan_TF)
+table(x.sa$hispan_TF, useNA = "ifany")
+prop.table(table(x.sa$hispan_TF, useNA = "ifany"))
+
+cam.design <- update(cam.design, hispanic_ = x.sa$hispan_TF)
+
+svytotal(~hispanic_, cam.design)
+confint(svytotal(~hispanic_, cam.design))
+
+svymean(~I(hispanic_ == "Hispanic"), cam.design)
+confint(svymean(~I(hispanic_ == "Hispanic"), cam.design))
 
 # Diagnosis breakdown
+# ADHD
+table(x.sa$add2, useNA = "if")
+x.sa$add2_r <- ifelse(x.sa$add2 > 2, NA, x.sa$add2)
+table(x.sa$add2, x.sa$add2_r, useNA = "if")
+
+cam.design <- update(cam.design,
+                     ADHD_ = factor(x.sa$add2_r, labels = c("ADHD", "no ADHD")))
+svytotal(~ADHD_, cam.design, na.rm = TRUE)
+
+# This does not totally work yet. Object is not found when trying to do the ratios
+svy_total_prop_CI <- function(var, true_label, design, ...) {
+  print(svytotal(as.formula(paste("~", var)), design, ...))
+  print(confint(svytotal(as.formula(paste("~", var)), design, ...)))
+  
+  print(svymean(~I(as.formula(var) == true_label), design, ...))
+  print(confint(svymean(~I(as.formula(var) == true_label), design, ...)))
+}
+svy_total_prop_CI("ADHD_", "ADHD", cam.design, na.rm = T)
