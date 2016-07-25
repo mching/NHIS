838d251268bdd5a5c8dd3be7ee1f9c9f01a92479
Nan Ketpura-Ching <Chings@Nans-iMac.local>
2016-07-24 16:51:45 -1000

    initial commit

---
+++ scripts/cam_topics.R
@@ -0,0 +1,18 @@
+# cam_topics.R
+# 
+# This file identifies the frequency of cam use and use in children with
+# specific health needs including developmental delay, autism, etc.
+
+# # Prerequisites:
+# Download childcam and samchild files and convert to RDA using this script:
+# https://github.com/mching/asdfree/blob/master/National%20Health%20Interview%20Survey/personsx%20plus%20samadult%20with%20multiple%20imputation%20-%20analyze.R
+# 
+# You also need to load and merge the sample child and child cam files using this script:
+# https://github.com/mching/NHIS/blob/master/scripts/load_and_merge_data.R
+# 
+# You also need to run the demographics script to create some of the recoded disability and other demographic variables
+# https://github.com/mching/NHIS/blob/master/scripts/demographics.R
+
+# Chiropractic
+# Ever used cco_use
+
