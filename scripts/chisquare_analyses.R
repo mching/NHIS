ea284bc47412e7b77827439d240e45000c58ac63
mching <mikeching@yahoo.com>
2016-07-30 17:34:17 -0600

    inital commit, all chisquares of the therapies vs diagnoses

---
+++ scripts/chisquare_analyses.R
@@ -0,0 +1,47 @@
+# chi-square testing
+
+svychisq(~MVI + isASD_, cam.design, na.rm = T)
+svychisq(~MVI + isID_, cam.design, na.rm = T)
+svychisq(~MVI + isDD_, cam.design, na.rm = T)
+svychisq(~MVI + isASDDDID_, cam.design, na.rm = T)
+
+svychisq(~ABCDE + isASD_, cam.design, na.rm = T)
+svychisq(~ABCDE + isID_, cam.design, na.rm = T)
+svychisq(~ABCDE + isDD_, cam.design, na.rm = T)
+svychisq(~ABCDE + isASDDDID_, cam.design, na.rm = T)
+
+svychisq(~MINERAL + isASD_, cam.design, na.rm = T)
+svychisq(~MINERAL + isID_, cam.design, na.rm = T)
+svychisq(~MINERAL + isDD_, cam.design, na.rm = T)
+svychisq(~MINERAL + isASDDDID_, cam.design, na.rm = T)
+
+svychisq(~ANYHERB + isASD_, cam.design, na.rm = T)
+# svychisq(~ANYHERB + isID_, cam.design, na.rm = T)
+svychisq(~ANYHERB + isDD_, cam.design, na.rm = T)
+svychisq(~ANYHERB + isASDDDID_, cam.design, na.rm = T)
+
+svychisq(~HOMEOPATHY + isASD_, cam.design, na.rm = T)
+# svychisq(~HOMEOPATHY + isID_, cam.design, na.rm = T)
+svychisq(~HOMEOPATHY + isDD_, cam.design, na.rm = T)
+svychisq(~HOMEOPATHY + isASDDDID_, cam.design, na.rm = T)
+
+# svychisq(~YOGA + isASD_, cam.design, na.rm = T)
+# svychisq(~YOGA + isID_, cam.design, na.rm = T)
+svychisq(~YOGA + isDD_, cam.design, na.rm = T)
+svychisq(~YOGA + isASDDDID_, cam.design, na.rm = T)
+
+# svychisq(~MEDITATION + isASD_, cam.design, na.rm = T)
+# svychisq(~MEDITATION + isID_, cam.design, na.rm = T)
+svychisq(~MEDITATION + isDD_, cam.design, na.rm = T)
+svychisq(~MEDITATION + isASDDDID_, cam.design, na.rm = T)
+
+# svychisq(~MASSAGE + isASD_, cam.design, na.rm = T)
+# svychisq(~MASSAGE + isID_, cam.design, na.rm = T)
+svychisq(~MASSAGE + isDD_, cam.design, na.rm = T)
+svychisq(~MASSAGE + isASDDDID_, cam.design, na.rm = T)
+
+# svychisq(~chiro + isASD_, cam.design, na.rm = T)
+# svychisq(~chiro + isID_, cam.design, na.rm = T)
+svychisq(~chiro + isDD_, cam.design, na.rm = T)
+svychisq(~chiro + isASDDDID_, cam.design, na.rm = T)
+
