803fb70735b5707fc3c2b7bc3f24d275e3870db6
mching <mikeching@yahoo.com>
2016-07-30 15:45:26 -0600

    initial commit

---
+++ scripts/cam_topics_ever.R
@@ -0,0 +1,25 @@
+# cam_topics_ever.R
+
+# Biologically Based
+# Diet
+# Chelation
+# Any vitamin, mineral
+# Multivitamin
+
+# Vitamin ABC...
+# Minerals
+# Any Herbal or supplement
+# Fish oil
+# Melatonin
+# Mind/Body
+# Native healer
+# Biofeedback
+# Manipulation and Body Based Methods
+# Massage
+# Chiropractic or Osteopathic
+# Hypnosis
+# Craniosacral
+# Alternative systems
+# Acupuncture
+# Ayurveda
+# Energy healing
