bf2aa794313c5e0634434ac2912ccd2da851054a
mching <mikeching@yahoo.com>
2016-09-29 16:28:05 -1000

    add additional models

--- logistic_regression.R
+++ logistic_regression.R
@@ -3,10 +3,13 @@ models_therapy <- function(therapy) {
   formula1 <- paste0(therapy, "~isASD_")
   formula2 <- paste0(therapy, "~isID_")
   formula3 <- paste0(therapy, "~isDD_")
+  formula4 <- paste0(therapy, "~isASDDDID_")
   asd_model <- svyglm(as.formula(formula1), design = cam.design, family = quasibinomial)
   id_model <- svyglm(as.formula(formula2), design = cam.design, family = quasibinomial)
   dd_model <- svyglm(as.formula(formula3), design = cam.design, family = quasibinomial)
-  return(list(asd = asd_model, id = id_model, dd = dd_model))
+  asdddid_model <- svyglm(as.formula(formula4), design = cam.design, 
+                          family = quasibinomial)
+  return(list(asd = asd_model, id = id_model, dd = dd_model, asdddid = asdddid_model))
   # extract coefficient
   # return OR and 95% CI
 }
@@ -16,10 +19,6 @@ OR_model <- function(model1) {
   exp(cbind(OR = coef(model1), confint(model1)))
 }
 
-x <- models_therapy("any_alt")
-lapply(x, summary)
-lapply(x, OR_model)
-
 x <- models_therapy("MVI")
 lapply(x, summary)
 lapply(x, OR_model)
@@ -27,3 +26,34 @@ lapply(x, OR_model)
 x <- models_therapy("ABCDE")
 lapply(x, summary)
 lapply(x, OR_model)
+
+x <- models_therapy("MINERAL")
+lapply(x, summary)
+lapply(x, OR_model)
+
+x <- models_therapy("ANYHERB")
+lapply(x, summary)
+lapply(x, OR_model)
+
+x <- models_therapy("any_alt")
+lapply(x, summary)
+lapply(x, OR_model)
+
+x <- models_therapy("chiro")
+lapply(x, summary)
+lapply(x, OR_model)
+
+x <- models_therapy("MASSAGE")
+lapply(x, summary)
+lapply(x, OR_model)
+
+x <- models_therapy("MEDITATION")
+lapply(x, summary)
+lapply(x, OR_model)
+
+x <- models_therapy("YOGA")
+lapply(x, summary)
+lapply(x, OR_model)
+
+
+#######
