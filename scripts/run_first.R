a83d994ec7d8a6a550dbd31205e2a57185060ff6
mching <mikeching@yahoo.com>
2016-10-03 15:11:45 -1000

    initial commit

---
+++ scripts/run_first.R
@@ -0,0 +1,8 @@
+# Run_first.R
+
+source('~/Dropbox/Mike/NHIS_CAM/scripts/load_and_merge_data.R')
+source('~/Dropbox/Mike/NHIS_CAM/scripts/helper_functions.R')
+source('~/Dropbox/Mike/NHIS_CAM/scripts/demographics.R')
+source('~/Dropbox/Mike/NHIS_CAM/scripts/demographics_ASDIDDD.R')
+source('~/Dropbox/Mike/NHIS_CAM/scripts/cam_topics.R')
+source('~/Dropbox/Mike/NHIS_CAM/scripts/cam_topics_ever.R')
